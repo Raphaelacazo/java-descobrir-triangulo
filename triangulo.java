627a52f5d2baa5f2afb648a603b26a65317b6315
Raphaelacazo <132139253+Raphaelacazo@users.noreply.github.com>
2023-04-29 16:52:25 -0300

    Add files via upload

---
+++ triangulo.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class valorTriangulo {
+ 
+ public static void main (String argd[]){
+
+Scanner  teclado = new Scanner (System.in);
+
+    int X1;
+    int Y1;
+    int Z2;
+	
+	String tipo       = "Isosceles"
+	String hipotenusa = "X"
+	
+	int continuar;
+	
+	if (X == Y && X){
+    tipo = "Equilatero"
+	hipotenusa = "Não tem hipotenusa,todos os lados são iguais";
+	{
+	
+	
+	
+	System.out.println("Uma hipotenusa tem um lado maior que o outro");
+	
+	System.out.println("Um equilatero tem todos os lados iguais");
+    
+	System.out.println("Um escaleno tem todos os lados iguais");
+	
+	if (X == Y && X) 
+	
+	if (X != Y && X != Z) 
+	
+	
+
+
+	
+			
+	
+	System.exit(0);
+		
+	}
+ }	
+	
+	
+	
+
